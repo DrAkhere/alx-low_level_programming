aa5b5df18652c0db7b9a338b299f8b649a8f55bd
DrAkhere <oziegbeakhere@gmail.com>
2023-03-19 13:01:41 -0700

    mite

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,15 +17,15 @@ int main(void)
 	{
 		for (y = x + 1; y <= '9'; y++)
 		{
-			for (z = x + 2; z <= '9'; z++)
+			for (z = y + 1; z <= '9'; z++)
 			{
-				if (x != y && x != z && y != z)
+				if ((x != y) != z)
 				{
 					putchar(x);
 					putchar(y);
 					putchar(z);
 
-					if (x == '7' && y == '8' && z == '9')
+					if (x == '7' && y == '8')
 						continue;
 
 					putchar(',');

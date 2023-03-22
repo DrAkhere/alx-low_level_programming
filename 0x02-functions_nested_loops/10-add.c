b2c6413d78bd69f85b1257f3d360ea954ad5c790
DrAkhere <oziegbeakhere@gmail.com>
2023-03-22 13:22:35 -0700

    mile

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry
+ * @int: firs and second integer
+ * Return: sum of the two intergers
+ */
+
+int add(int x, int y)
+{
+       	int sum;
+
+	sum = x + y;
+	return (sum);
+}

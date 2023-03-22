448b833e5597e8108b5788d39f9bdcdc2862783f
DrAkhere <oziegbeakhere@gmail.com>
2023-03-22 02:30:50 -0700

    mit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - Entry
+ * print_alphabet_x10 - Entry
  * return: Always 0
  */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (i = 'a'; 1 <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
+}

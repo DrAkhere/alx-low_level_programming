154201541a680a2142cbfa0b8f2bff1a2334e989
DrAkhere <oziegbeakhere@gmail.com>
2023-03-27 13:36:02 -0700

    good

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point of a function that takes a pointer to an integer as parameter and update the value to 98
+ * @n: new value of pointer
+ * Return: return nothing at this point
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Entry point to swap the value of two integers
+ * @a: integer to be swapped or replaced
+ * @b: integer replacing the swapped integer
+ * Return: return nothing at this point
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

66bbc900075584cee863716bfab7576044af3891
DrAkhere <oziegbeakhere@gmail.com>
2023-03-27 13:43:18 -0700

    good

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - Entry point of a function that takes a pointer to an integer as parameter and update the value to 98
- * @n: new value of pointer
+ * reset_to_98 - Entry point
+ * @n: integer to replace value of pointer
  * Return: return nothing at this point
  */
 

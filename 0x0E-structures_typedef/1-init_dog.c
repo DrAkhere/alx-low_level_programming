02d32e7215f7409d9ca51d8d14fa494bf629ad9c
DrAkhere <oziegbeakhere@gmail.com>
2023-04-17 18:50:30 -0700

    description

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
-
+#include <stdlib.h>
+ /**
+  * init_dog - Entry point to define dog data
+  * @d: dog identity
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: name of owner of dog
+  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{
+/**
+ * struct dog - Entry pont to dog data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

7cd0ce1ba8a512d483c5bf64b3538f82945c2ea4
DrAkhere <oziegbeakhere@gmail.com>
2023-04-19 16:36:19 -0700

    checking file presence

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,32 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  * main - define function
-  * @void - describe argument
-  * Return: what does it return?
-  */
-int main(void)
+ * main - Entry point for function
+ * @argc: The argument counter
+ * @argv: The argument vector
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
 {
+	int x = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (; x < atoi(argv[1]); x++)
+	{
+		printf("%02hhx", *((char *)main + x));
+		if (x < atoi(argv[1]) - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }

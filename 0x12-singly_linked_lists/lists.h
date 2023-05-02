3c9bb5d52077feb358968118a3b7237c2aa96880
DrAkhere <oziegbeakhere@gmail.com>
2023-05-02 14:31:35 -0700

    bety correction lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,8 @@
 #define LISTS_H
 #include <stddef.h>
 /**
- * struct list_s - singly linked list        * @str: string - (malloc'ed string)
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *

1934b413cfff63de20d25bf941c3187ba2347f03
DrAkhere <oziegbeakhere@gmail.com>
2023-05-03 01:54:13 -0700

    Correcting 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,11 +10,13 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t count;
-	
+
 	if (h == NULL)
 		return (0);
-	count = 0;
-	for (h != NULL, h = h->next; count++))
+	for (count = 0; h != NULL; h = h->next)
+	{
+		count++;
 		;
+	}
 	return (count);
 }

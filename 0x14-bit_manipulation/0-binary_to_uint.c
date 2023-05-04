146b2dafa769be337721a8392ae2d25f31a2b276
DrAkhere <oziegbeakhere@gmail.com>
2023-05-04 07:16:21 -0700

    correct betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,11 +10,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int unit = 0;
 	int x;
 
+	if (b == NULL)
+		return (0);
 	for (x = 0; b[x] != '\0'; x++)
 	{
 		if (b[x] != '0' && b[x] != '1')
 			return (0);
-		else if (b[x] =='1')
+		else if (b[x] == '1')
 			unit = (unit << 1) | 1;
 		else if (b[x] == '0')
 			unit <<= 1;
